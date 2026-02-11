417f42cff3210c2df5683113be20eb33e8534849
cashok1535 <cashok1928@gmail.com>
2026-02-11 14:08:45 +0200

    add fonts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "/Users/aleksandrglebov/Sites/Car-cosmetics/src/css/index.css";
+import "../src/css/index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
